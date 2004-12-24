30ab6f6cfacc3b7a054122b21a1f34d001f308c2
Daisuke Maki <dmaki@cpan.org>
2004-12-24 04:10:11 +0000

    Import of DMAKI/POE-Component-StackedProcessor-0.02 from CPAN.
    gitpan-cpan-distribution: POE-Component-StackedProcessor
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         DMAKI/POE-Component-StackedProcessor-0.02.tar.gz
    gitpan-cpan-author:       DMAKI
    gitpan-cpan-maturity:     released

--- lib/POE/Component/StackedProcessor.pm
+++ lib/POE/Component/StackedProcessor.pm
@@ -1,63 +1,89 @@
-# $Id: StackedProcessor.pm,v 1.7 2004/12/10 22:48:04 daisuke Exp $
+# $Id: StackedProcessor.pm 3 2004-12-24 03:59:51Z daisuke $
 #
-# Daisuke Maki <daisuke@cpan.org>
+# Daisuke Maki <dmaki@cpan.org>
 # All rights reserved.
 
 package POE::Component::StackedProcessor;
 use strict;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use base qw(Class::Data::Inheritable);
 use Class::MethodMaker
     new_with_init  => 'new',
     new_hash_init  => 'hash_init',
-    list           => [ qw(Processors) ],
-    get_set        => [ qw(OnSuccess OnFailure) ],
+    list           => [ qw(alias processors) ],
+    get_set        => [ qw(on_success on_failure) ],
 ;
-use Params::Validate qw(validate validate_with ARRAYREF CODEREF SCALAR);
+use Params::Validate ();
 use POE;
 
 __PACKAGE__->mk_classdata(qw(callback_name));
 __PACKAGE__->callback_name('process');
 
 my %InitValidate = (
-    Alias         => { type => SCALAR,   default => 'SP' },
-    Processors    => { type => ARRAYREF, default => [] },
-    ProcessorArgs => { type => ARRAYREF, default => [] },
-    OnSuccess     => { type => CODEREF,  default => \&noop_success },
-    OnFailure     => { type => CODEREF,  default => \&noop_fail    },
+    Alias         => { type => Params::Validate::SCALAR(),   default => 'SP' },
+    Processors    => { type => Params::Validate::ARRAYREF(), default => [] },
+    InlineStates  => { type => Params::Validate::HASHREF(),  optional => 1 },
+    PackageStates => { type => Params::Validate::HASHREF(),  optional => 1 },
+    ObjectStates  => { type => Params::Validate::HASHREF(),  optional => 1 },
 );
+
+sub _InitArgNormalizer
+{
+    $_[0] =~ s/^([[:upper:]])/\L\1\E/;
+    $_[0] =~ s/(?<=[[:lower:]])([[:upper:]])/_\L\1\E/g;
+    $_[0];
+}
+
 sub init
 {
     my $self = shift;
-    my %args  = validate(@_, \%InitValidate);
+    my %args  = Params::Validate::validate_with(
+        params => \@_,
+        spec   => \%InitValidate,
+        normalize_keys => \&_InitArgNormalizer
+    );
 
-    if (scalar @{$args{Processors}}) {
-        validate_with(
-            params => $args{Processors},
-            spec   => [ ({ can => $self->callback_name }) x scalar @{$args{Processors}} ],
+    if (scalar @{$args{processors}}) {
+        Params::Validate::validate_with(
+            params => $args{processors},
+            spec   => [ ({ can => $self->callback_name }) x scalar @{$args{processors}} ],
             called => ref($self) . "::new"
         );
     }
 
-    my $alias   = delete $args{Alias};
-    my $pr_args = delete $args{ProcessorArgs};
+    my $alias   = delete $args{alias};
+    my $pr_args = delete $args{processor_args};
+
+    my %states = (
+        inline_states => { _start => \&_start, _stop => \&_stop },
+        object_states => [ $self, [ qw(process run_processor) ] ]
+    );
+    foreach my $state qw(inline_states package_states object_states) {
+        if (exists $args{$state}) {
+            my $user_supplied = delete $args{$state};
+            if (my $value = $states{$state}) {
+                $states{$state} = $state eq 'inline_states' ?
+                    { %$value, %$user_supplied } :
+                    [ @$user_supplied, @$value ];
+            } else {
+                $states{$state} = $user_supplied;
+            }
+        }
+    }
 
     $self->hash_init(%args);
-        
+
     POE::Session->create(
-        object_states => [
-            $self, [ qw(_start _stop next_processor) ]
-        ],
-        heap => { args => $pr_args },
-        args => [ $alias ],
+        heap  => { args => $pr_args },
+        args  => [ $alias ],
+        %states
     );
 }
 
 sub _start
 {
-    my ($kernel, $alias) = @_[KERNEL, ARG0];
+    my($kernel, $alias) = @_[KERNEL, ARG0];
     $kernel->alias_set($alias);
-    $kernel->yield('next_processor', 0);
 }
 
 sub _stop
@@ -66,34 +92,44 @@ sub _stop
     $kernel->alias_remove();
 }
 
-sub next_processor
+sub process
 {
-    my($self, $pr_idx, $heap) = @_[OBJECT, ARG0, HEAP];
+    my($self, $kernel, $session, $success_evt, $failure_evt, $input) =
+        @_[OBJECT, KERNEL, SESSION, ARG0, ARG1, ARG2];
 
-    my $pr    = $self->Processors_index($pr_idx);
+    $kernel->yield(
+        'run_processor',
+        $session->postback($success_evt, $input), 
+        $session->postback($failure_evt, $input),
+        $input,
+        0
+    );
+}
+
+sub run_processor
+{
+    my($self, $kernel, $success_evt, $failure_evt, $input, $pr_idx) =
+        @_[OBJECT, KERNEL, ARG0, ARG1, ARG2, ARG3, ARG4];
+    my $pr    = $self->processors_index($pr_idx);
     my $cb    = $self->callback_name();
     my $ret   = eval {
         # XXX - need to figure out what to pass to this guy
-        $pr->$cb(@{ $heap->{args} }); 
+        $pr->$cb($input); 
     };
     warn if $@;
 
     if ($@ || !$ret) {
         warn if $@;
-        $self->OnFailure();
+        $failure_evt->($input);
     } else {
         my $next = $pr_idx + 1;
-        if ($self->Processors_count() > $next) {
-            return POE::Kernel->yield('next_processor', $next);
+        if ($self->processors_count() > $next) {
+            $kernel->yield('run_processor', $success_evt, $failure_evt, $input, $next);
         } else {
-            $self->OnSuccess();
+            $success_evt->($input);
         }
     }
-    POE::Kernel->yield();
 }
-
-sub noop_success { warn "Success" }
-sub noop_fail    { warn "Fail"    }
 
 1;
 
@@ -101,7 +137,7 @@ __END__
 
 =head1 NAME
 
-POE::Component::StackedProcessor - Stack Processors In POE
+POE::Component::StackedProcessor - Stacked Processors In POE
 
 =head1 SYNOPSIS
 
@@ -109,11 +145,15 @@ POE::Component::StackedProcessor - Stack Processors In POE
   POE::Component::StackedProcessor->new(
     Alias      => $alias,
     Processors => [ $proc1, $proc2, $proc3 ],
-    OnSuccess  => \&callback,
-    OnFailure  => \&callback,
-    ProcessorArgs => [ $foo, $bar, $baz ],
+    InlineStates => {  # or PackageStates/ObjectStates
+      success => \&success_cb,
+      failure => \&failure_cb
+    }
   );
   POE::Kernel->run();
+
+  # else where in the code...
+  $kernel->post($alias, 'process', 'success', 'failure', $input);
 
 =head1 DESCRIPTION
 
@@ -123,54 +163,96 @@ processor.
 
 For example, suppose you have an HTML document that requires you to verify
 whethere it meats certain criteria such as proper markup, valid links, etc.
-
-All you need to do is to create objects that have a method "process", and
-then you can invoke this from the stacked processor.
-
-  package CheckMarkup;
-  sub new { ... }
-  sub process { ... }
-
-  package ValidateLinks;
-  sub new { ... }
-  sub process { ... }
-
-  package main;
-  my $cm = CheckMarkup->new;
-  my $vl = ValidateLink->new;
-  POE::Component::StackedProcessor->new(
-    Processors => [ $cm, $vl ],
-  );
-
 Normally this would be done in one pass for the sake of efficiency, but
 sometimes you want to break these steps up into several components such that
 you can mix and match the differnt processors as required.
 
-=head1 METHOD
+The basic steps to creating a stacked processor is as follows:
+
+=over 4
+
+=item 1. Create some processors
+
+These are just simple objects that have a method called "process". The method
+should take exactly one parameter, which is the "thing" being processed,
+whatever it may be. It must return a true value upon successful execution.
+If an exception is thrown or the method returns a false value, the processing
+is terminated right there and then, and the failure event will be called.
+
+Once you define these processors, pass them to the Processors argument to
+new(), in the order that you want them executed:
+
+  POE::Component::StackedProcessor->new(
+    ...
+    Processors => [ $p1, $p2, $p3 ... ]
+  );
+
+=item 2. Define success and failure events
+
+You need to define success and failure events so that upon completion of
+executing the processors, you can do some post processing. You specify
+which states get called.
+
+  # Calling from outside a POE session:
+  sub success_cb { ... }
+  sub failure_cb { ... }
+  POE::Component::StackedProcessor->new(
+    ...,
+    InlineStates => {  # or PackageStates/ObjectStates
+      success => \&success_cb,
+      failure => \&failure_cb
+    }
+  );
+
+Because the success/failure events are invoked via POE::Kernel's post() method,
+they will receive the "request" and "response" arguments in ARG0 and ARG1,
+which are arrayrefs:
+
+  sub success_cb {
+    my($response, $response) = @_[ARG0, ARG1];
+    # whatever that got passed to process() is in $response-E<gt>[0];
+    ...
+  }
+
+=item 3. Send some data to be processed
+
+Once you've set up the processors and the success/failure states,
+send some data to the StackedProcessor session via POE::Kernel->post()
+
+  POE::Kernel->post(
+    $alias_of_stacked_processor,
+    'process', # this is always the same
+    $success_event,
+    $failure_event,
+    $arg_to_process
+  );
+
+If all processors complete execution successfully, $success_event will be
+called. Otherwise $failure_event gets called.
+
+=back
+
+=head1 METHODS
 
 =head2 new ARGS
 
 =over 4
+
+=item Alias
+
+Alias of the StackedProcessor session. (You probably want to set this, because
+you will need to post() to this session to run the processors)
 
 =item Processors
 
 An arrayref of processor objects. Each of these object must have a method
 denoted by "callback_name" (which is by default "process")
 
-=item ProcessArgs
+=item InlineStates/Packagestates/ObjectStates
 
-An arrayref of arguments that should be passed to the processors. 
-
-=item OnSuccess / OnFailure
-
-Specify a callback to be executed on completion of processing. You must pass
-a reference to a code ref.
-
-If you want events to be posted to a session or such, pass a postback code
-
-  POE::Component::StackedProcessor->new(
-    OnSuccess => $session->postback('my event')
-  );
+Optional states to faciliate your method dispatching. You can, for example,
+set success and failure states there that gets called upon successful
+execution.
 
 =back
 
@@ -185,6 +267,6 @@ L<POE>
 
 =head1 AUTHOR
 
-Daisuke Maki E<lt>daisuke@cpan.orgE<gt>
+Daisuke Maki E<lt>dmaki@cpan.orgE<gt>
 
 =cut
--- t/01-sanity.t
+++ t/01-sanity.t
@@ -7,25 +7,61 @@ package NoX;
 use strict;
 sub new { bless {}, shift }
 sub process {
-    my $self = shift;
-    my $text_ref = shift;
-    $$text_ref =~ s/x/_/ig
+    my $self  = shift;
+    my $input = shift;
+
+    $input->{text} =~s/x//ig;
+    return 1;
 }
+
+package EightChars;
+use strict;
+sub new { bless {}, shift }
+sub process {
+    my $self  = shift;
+    my $input = shift;
+    return length($input->{text}) <= 8;
+}
+        
 
 package main;
 use strict;
+use POE;
 
-my $text = "axbxcxdxexfxgxhxixjxk";
+my @inputs = (
+    { ok_to_fail => 1, text => 'axbxcxdxexfxgxhxixjxk' },
+    { ok_to_fail => 0, text => ' axxb' }
+);
 my $nox  = NoX->new;
-my $p = POE::Component::StackedProcessor->new(
-    Processors => [ $nox ],
-    ProcessorArgs => [ \$text ],
-    OnSuccess     => \&success
+my $chr  = EightChars->new;
+
+POE::Component::StackedProcessor->new(
+    Alias => "sp",
+    Processors => [ $nox, $chr ],
+    InlineStates => {
+        success => \&success,
+        failure => \&failure,
+    }
 );
 
 sub success
 {
-    warn "foo";
+    my($response) = @_[ARG1];
+    my $input = $response->[0];
+    ok(!$input->{ok_to_fail}, "Processed input");
+    ok($input->{text} !~ /x/, "No X's");
+    ok(length($input->{text}) <= 8, "Less than 8 chars");
+}
+
+sub failure
+{
+    my($response) = @_[ARG1];
+    my $input = $response->[0];
+    ok($input->{ok_to_fail}, "Processed input");
+}
+
+foreach my $input (@inputs) {
+    POE::Kernel->post('sp', 'process', 'success', 'failure', $input);
 }
 
 POE::Kernel->run();
